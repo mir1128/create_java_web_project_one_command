8fe205916492e8735d09b82e0a9de64596f01c77
jie.liu <jieliu@thoughtworks.com>
2015-12-11 17:08:47 +0800

    update

--- spring_web_project_archetype/src/main/resources/archetype-resources/src/test/java/CDPlayerTest.java
+++ spring_web_project_archetype/src/main/resources/archetype-resources/src/test/java/CDPlayerTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {DevelopmentProfileConfig.class, ProductionProfileConfig.class})
+@ContextConfiguration(classes = {DevelopmentProfileConfig.class})
 public class CDPlayerTest {
 
     @Rule
